8b8adb48a98c9e5a33146a5d91de20da6b4fb1f8
owo12321 <58645453+owo12321@users.noreply.github.com>
2024-03-08 05:03:24 +0800

    Add files via upload

---
+++ upper_computer/car_description/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'car_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='owo',
+    maintainer_email='owo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
